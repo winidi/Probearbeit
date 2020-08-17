c44c4053ad2969846c499dd6f2e6e0d22d47614f
Dirk <dirkwiniecki@web.de>
2020-08-17 12:35:13 +0200

    Commit2

--- pages/kontakt.vue
+++ pages/kontakt.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="content">
-
+        <div class="text">
         <h1>Kontaktdaten</h1>
         <p>
             <strong> LeosRestaurant </strong>
@@ -14,6 +14,7 @@
 
         <p>Email: <a href="mailto:dirkwiniecki@web.de">dirkwiniecki@web.de</a></p>
         <p>Internet: <a href="https://github.com/winidi/Probearbeit1708">https://github.com/winidi/Probearbeit1708</a></p>
+        </div>
     </div>
 </template>
 
@@ -21,11 +22,20 @@
 
 .content{  
     margin: auto;
-    width: 50%;
+    width: 100%;
+    
+    background-image: linear-gradient(
+        #f8bcb8,	
+        rgb(212, 179, 179),    
+        #B5D5D8);
 }
 
 p {
     text-align: left;
     font-size: 1.2em;
+}
+
+.text{
+    margin-left: 14em;
 }
 </style>
